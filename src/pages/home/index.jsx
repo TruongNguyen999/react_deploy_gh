0ab2b9d20fd081cf7b4194ad4b34ef0a619eddad
truong.nguyen@edugix.com <truong.nguyen@edugix.com>
2023-12-20 20:52:45 +0700

    change text

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const Home = () => {
     return <div className="screenBlue">
-            <Link to={`move-task`}>Move task asd sd</Link>
+            <Link to={`move-task`}>Move task</Link>
         </div>
 }
 
